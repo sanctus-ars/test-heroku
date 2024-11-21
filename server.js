28abc16b5c432307b7339757dc91f328d499bf7e
romanlavrinjuk <romanrostislavovich@gmail.com>
2024-11-21 20:21:09 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/fe/browser'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<app-name>/index.html'));});
+    '/dist/fe/browser/index.html'));});
 app.listen(process.env.PORT || 8080);
